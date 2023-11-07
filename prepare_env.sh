37e1bae0cf4b65453d07d762fa6da212a2928b3f
alex-tabachuk <a.tabachuk@geniusee.com>
2023-11-07 14:53:11 +0200

    fixes

--- prepare_env.sh
+++ prepare_env.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
+set -e
+
+if [ -z "$AWS_DEFAULT_REGION" ]; then
+    echo "AWS_DEFAULT_REGION is not set. It is required to set this variable!"
+    exit 1
+else
+    echo "AWS_DEFAULT_REGION is set to $AWS_DEFAULT_REGION."
+fi
+
 apt update && apt install -y curl wget unzip python3 python3-pip postgresql-client
 
 snap install aws-cli --classic
+
+pip3 install -r requirements.txt
+
+read -p "Node Home Domain: " home_domain
+read -p "Node Seed: " node_seed
+read -p "Node Name: " node_name
+
+python3 generator.py $home_domain $node_seed $node_name > /etc/stellar.cfg
 
 DISTRO=$(lsb_release -cs)
  wget -qO - https://apt.stellar.org/SDF.asc | apt-key add -
@@ -28,4 +45,4 @@ EOF
 systemctl daemon-reload
 systemctl enable stellar-core.service
 
-echo "Done. Don't forget to set AWS_DEFAULT_REGION"
+echo "Done."
